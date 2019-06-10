66a5a2ebf99ad35b7218d3c622eadcb75dbaf8ad
Tristan Ross <tgr1998@live.com>
2019-06-10 15:24:49 -0700

    Complete stock.rb

---
+++ stock.rb
@@ -0,0 +1,16 @@
+def stock_picker(arr)
+    max = 0
+    indexOne = 0
+    indexTwo = 0
+    for x in 0..arr.length - 1
+        for y in x..arr.length - 1
+            if arr[y] - arr[x] > max
+                indexOne = x
+                indexTwo = y
+                max = arr[y] - arr[x]
+            end
+        end
+    end
+    puts "[#{indexOne},#{indexTwo}]"
+end
+stock_picker([5, 4, 3])
